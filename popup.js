cf684474dd3600fd07ca9c082ddb4b1e0b2be1fd
rlacobelle20 <44063772+rlacobelle20@users.noreply.github.com>
2020-07-28 10:31:35 -0400

    Add files via upload
    Files added for popup- not completed, only uploaded so they would be on github

---
+++ popup.js
@@ -0,0 +1,15 @@
+// coding for the popup
+
+// codes for the hex number
+// places the color into the 
+
+function setup() {
+    backgroundColor = 225;
+    c = color(HSB, 360, 100, 100);
+}
+
+function draw() {
+    background(backgroundColor);
+    fill(c)
+    ellipse();
+}
