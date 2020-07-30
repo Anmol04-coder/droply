73b6963d40e8fc4e222adb29769217c1580686be
Rachel Lacobelle <44063772+rlacobelle20@users.noreply.github.com>
2020-07-30 00:26:12 -0400

    Add files via upload
    upload of glitch code

---
+++ src/converter.js
@@ -0,0 +1,8 @@
+function componentToHex(c) {
+  let hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+console.log(rgbToHex(28, 135, 201)); // #1c89c9
