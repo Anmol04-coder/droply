4e9ecab4ed27a24b3ee4675c47819ea62477e243
Rachel Lacobelle <44063772+rlacobelle20@users.noreply.github.com>
2020-07-30 00:41:16 -0400

    Delete converter.js
    trying to find a fix- console won't work anymore with this

--- src/converter.js
+++
@@ -1,8 +0,0 @@
-function componentToHex(c) {
-  let hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-console.log(rgbToHex(28, 135, 201)); // #1c89c9
