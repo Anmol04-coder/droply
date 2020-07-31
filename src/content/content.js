97d9ffe4c11448c7095f2ee0f37326cd0468b32e
sviho <svitlanahoncharukk@gmail.com>
2020-07-31 12:25:53 +0300

    moved iframe-related code to iframe.js

--- src/content/content.js
+++ src/content/content.js
@@ -4,10 +4,7 @@ let tempContext = tempCanvas.getContext('2d');
 // Stop the pointer events to prevent accidental button clicking when picking a color
 document.body.style['pointer-events'] = 'none';
 
-let iframe = document.createElement('iframe');
-let iframeDocument;
-setupIframe(0, 0, 0);
-
+// Listen for clicks on the page and call capturing once the event happens
 document.addEventListener("click", captureCurrentPixel, false);
 
 function captureCurrentPixel(e) {
@@ -35,69 +32,4 @@ function getColor(x, y) {
 	setupIframe(red, green, blue);
 
 	document.body.style['pointer-events'] = 'auto';
-}
-
-function setupIframe(r, g, b) {
-	iframe.onload = function() {
-		iframe.style.background = "#EEEEEE";
-		iframe.style.height = "50%";
-		iframe.style.width = "180px";
-		iframe.style.position = "fixed";
-		iframe.style.top = "0px";
-		iframe.style.right = "0px";
-		iframe.style.zIndex = "1000000000";
-		iframe.frameBorder = "none";
-
-		iframeDocument = iframe.contentDocument;
-
-		showClose();
-		showRect(r, g, b);
-		showParagraph(`RGB: (${r}, ${g}, ${b})`);
-		showParagraph(`HEX: ${rgbToHex(r, g, b)}`);
-		showParagraph(`HSL: ${rgbToHsl(r, g, b)}`);
-
-		iframeDocument.body.style.fontFamily = 'Open Sans';
-		iframeDocument.body.style.fontWeight = '200';
-    iframeDocument.body.style.color = '#333333';
-		iframeDocument.body.style.paddingLeft = "10px";
-		iframeDocument.body.style.overflow = "hidden";
-	}
-	document.body.appendChild(iframe);
-}
-
-function showClose() {
-	let closeButton = iframeDocument.createElement("p");
-	let closeSymbol = iframeDocument.createTextNode("✕");
-	closeButton.style.textAlign = "right";
-	closeButton.onclick = stopExtension;
-	closeButton.appendChild(closeSymbol);
-
-	iframeDocument.body.appendChild(closeButton);
-}
-
-function showRect(r, g, b) {
-	let c = iframeDocument.createElement('canvas');
-	let ctx = c.getContext("2d");
-
-	ctx.beginPath();
-	ctx.fillStyle = `rgb(${r}, ${g}, ${b})`
-	ctx.rect(10, 10, 100, 100);
-	ctx.rect.lineWidth = "5px";
-	ctx.rect.strokeStyle = "black";
-	ctx.stroke();
-	ctx.fill();
-
-	iframeDocument.body.appendChild(c);
-}
-
-function showParagraph(text) {
-	let paragraph = iframeDocument.createElement("p");
-	let textElement = iframeDocument.createTextNode(text);
-	paragraph.appendChild(textElement);
-
-	iframeDocument.body.appendChild(paragraph);
-}
-
-function stopExtension() {
-	iframe.parentNode.removeChild(iframe);
 }
---
+++ src/content/iframe.js
@@ -0,0 +1,74 @@
+let iframe = document.createElement('iframe');
+let iframeDocument;
+
+setupIframe(0, 0, 0);
+
+function setupIframe(r, g, b) {
+	iframe.onload = function() {
+		iframe.style.background = "#EEEEEE";
+		iframe.style.height = "50%";
+		iframe.style.width = "180px";
+		iframe.style.position = "fixed";
+		iframe.style.top = "0px";
+		iframe.style.right = "0px";
+		iframe.style.zIndex = "1000000000";
+		iframe.frameBorder = "none";
+		iframe.id = 'droplyIframe';
+
+		iframeDocument = iframe.contentDocument;
+
+		showClose();
+		showRect(r, g, b);
+		showParagraph(`RGB: (${r}, ${g}, ${b})`);
+		showParagraph(`HEX: ${rgbToHex(r, g, b)}`);
+		showParagraph(`HSL: ${rgbToHsl(r, g, b)}`);
+
+		iframeDocument.body.style.fontFamily = 'Open Sans';
+		iframeDocument.body.style.fontWeight = '200';
+    iframeDocument.body.style.color = '#333333';
+		iframeDocument.body.style.paddingLeft = "10px";
+		iframeDocument.body.style.overflow = "hidden";
+	}
+	document.body.appendChild(iframe);
+}
+
+function showClose() {
+	let closeButton = iframeDocument.createElement("p");
+	let closeSymbol = iframeDocument.createTextNode("✕");
+	closeButton.style.textAlign = "right";
+	closeButton.onclick = stopExtension;
+	closeButton.appendChild(closeSymbol);
+
+	iframeDocument.body.appendChild(closeButton);
+}
+
+function showRect(r, g, b) {
+	let c = iframeDocument.createElement('canvas');
+	let ctx = c.getContext("2d");
+
+	ctx.beginPath();
+	ctx.fillStyle = `rgb(${r}, ${g}, ${b})`
+	ctx.rect(10, 10, 100, 100);
+	ctx.rect.lineWidth = "5px";
+	ctx.rect.strokeStyle = "black";
+	ctx.stroke();
+	ctx.fill();
+
+	iframeDocument.body.appendChild(c);
+}
+
+function showParagraph(text) {
+	let paragraph = iframeDocument.createElement("p");
+	let textElement = iframeDocument.createTextNode(text);
+	paragraph.appendChild(textElement);
+
+	iframeDocument.body.appendChild(paragraph);
+}
+
+function stopExtension() {
+	iframe.parentNode.removeChild(iframe);
+
+	for (let i = 0; i < children.length; i++) {
+		children[i].style['pointer-events'] = 'auto';
+	}
+}
